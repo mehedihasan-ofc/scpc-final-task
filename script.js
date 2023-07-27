575dc07a061ebb7176ee0eb06741364846e5b9e2
mehedihasan-ofc <mehedi.ofcc@gmail.com>
2023-07-27 14:21:07 +0600

    task 5 problem solved

--- script.js
+++ script.js
@@ -58,3 +58,22 @@ const findTwoNumbersWithSum = (arr, target) => {
 
     return null; // If no such pair is found, return null
 };
+
+// Task 5: Implement a simple JavaScript calculator. The calculator should take two numbers and an operator (+, -, *, /) as input and return the result of the operation.
+
+const calculator = (num1, num2, operator) => {
+    if (operator === '+') {
+        return num1 + num2;
+    } else if (operator === '-') {
+        return num1 - num2;
+    } else if (operator === '*') {
+        return num1 * num2;
+    } else if (operator === '/') {
+        if (num2 === 0) {
+            return 'Error: Division by zero is not allowed.';
+        }
+        return num1 / num2;
+    } else {
+        return 'Error: Invalid operator.';
+    }
+};
