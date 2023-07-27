a8cef79de62c2ace86d85c758da4cab8906c4224
mehedihasan-ofc <mehedi.ofcc@gmail.com>
2023-07-27 14:22:43 +0600

    task 6 problem solved

--- script.js
+++ script.js
@@ -77,3 +77,22 @@ const calculator = (num1, num2, operator) => {
         return 'Error: Invalid operator.';
     }
 };
+
+// Task 6: Create a program that generates a random password of a specified length. The password should include a mix of uppercase letters, lowercase letters, numbers, and special characters.
+
+const generateRandomPassword = (length) => {
+    const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+    const numberChars = "0123456789";
+    const specialChars = "!@#$%^&*()_-+=<>?/{}[]|";
+
+    const allChars = uppercaseChars + lowercaseChars + numberChars + specialChars;
+
+    let password = '';
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * allChars.length);
+        password += allChars[randomIndex];
+    }
+
+    return password;
+};
